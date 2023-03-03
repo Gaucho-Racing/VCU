f2887beb1a00f85e5c16915e718cdc8021311369
Rick T. Zhang <rtzhang@ucsb.edu>
2023-03-03 04:30:06 +0000

    setup + main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
+//main.cpp
+// @rt.z
+
 #include <Arduino.h>
+
+enum States = {OFF, ON, ON_READY, DRIVE, CHARGE_PRECHARGE, CHARGE_CHARGING, CHARGE_FULL};
+volatile States state;
+
+//placeholder #includes for until the actual files come out
+/*
+#include "on_off.cpp"
+#include "drive.cpp"
+#include "charge.cpp"
+*/
+
 void setup() {
-// write your initialization code here
+   state = OFF;
 }
 
 void loop() {
-// write your code here
+   switch (state) {
+      case OFF:
+         state = OFF();
+      case ON:
+         state = ON();
+      case ON_READY:
+         state = ON_READY();
+      case DRIVE:
+         state = DRIVE();
+      case CHARGE_PRECHARGE:
+         state = CHARGE_PRECHARGE();
+      case CHARGE_CHARGING():
+         state = CHARGE_CHARGING();
+      case CHARGE_FULL():
+         state = CHARGE_FULL();
+   }
 }
