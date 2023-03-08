0c122aed0e8c6eeaaf8169300037a1dd7ffd3f3c
Nikunj Parasar <nikunj.parasar@gmail.com>
2023-03-08 13:25:54 -0800

    more notes regarding arm-m7 teensy architecture

--- src/InterruptsTEST.cpp
+++ src/InterruptsTEST.cpp
@@ -29,3 +29,27 @@ void __attribute__((interrupt("IRQ"))) my_interrupt_handler(void)
 {
     // Handle the interrupt here
 }
+
+
+/*
+Here's a breakdown of the code:
+
+#include <stdint.h> and "stm32f7xx.h" include the necessary header files.
+            USE imxrt.h INSTEAD OF stm32f7xx.h ABOVE
+            
+#define MY_INTERRUPT_PRIORITY 0x01 defines the interrupt priority level. In this example, we set it to the lowest priority (0x01).
+
+#define MY_INTERRUPT_NUMBER 0x01 defines the interrupt number. In this example, we set it to 1.
+
+NVIC_EnableIRQ((IRQn_Type)MY_INTERRUPT_NUMBER) enables the interrupt.
+
+NVIC_SetPriority((IRQn_Type)MY_INTERRUPT_NUMBER, MY_INTERRUPT_PRIORITY) sets the interrupt priority level.
+
+while (1) {...} triggers the software interrupt repeatedly in an infinite loop.
+
+SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk triggers the software interrupt by setting the PendSV flag in the Interrupt Control and State Register (ICSR).
+
+void __attribute__((interrupt("IRQ"))) my_interrupt_handler(void) is the interrupt service routine for the software interrupt. It handles the interrupt when it occurs.
+
+
+*/
