169606d38233ad637aace207f6a241b3ff4ee225
Karankumar Mageswaran <mageswarankk@gmail.com>
2023-03-09 20:22:05 -0800

    udpates

--- src/drive.cpp
+++ src/drive.cpp
@@ -4,38 +4,41 @@ States drive() {
   while(1) {
     
     // if throttle not applied
-    if(getThrottleIn() == 0) {
+    if(getAPPS() == 0) {
       return ON_READY;
     }
     
-    // brake pedal plausibility check
-    if(getThrottleIn() > 25 && getBrakeIn() > 0) {
+    // brake system plausibility check
+    // temporary or redundant
+    // getBrakePressure ????
+    if(getAPPS() > 25 && getBrakeIn() > 0) {
       setRcurrent(0);
-      return OFF;
+      return OFF; // STUB - will replace with interrupt
     }
     
     // set motor output
-    setRCurrent(motorOut(getThrottleIn());   
+    setRCurrent(motorOut(getAPPS());   
   
   }
 }
 
-float motorOut(float throttle ) {
+float motorOut(float throttle) {
   // i'm assuming throttle is a value between 0 and 100
   // can adjust accordingly later
   
-  // figure out range from getXXspeed()
+  // figure out range/scaling from getXXspeed()
   float front = ( getFRspeed() + getFFspeed() ) / 2; // CAN typo lol, should be FL not FF
-  float rear = ( getRRspeed() + getRLspeed() ) / 2;
+  float rear = ( getRRspeed() + getRLspeed() ) / 2; // MIGHT CHANGE TO MOTOR SPEED IN FUTURE
 
   // wheels slipping: traction control
+  // multiplied by 10 is because of some CAN scaling shit
   if (rear > front) {
     // adjust 0.1 factor in testing
-    return throttle - 0.1 * (rear - front);
+    return 10 * (throttle - 0.1 * (rear - front));
   }
 
   else {
-    return throttle;
+    return 10 * (throttle);
   }
   
 }
