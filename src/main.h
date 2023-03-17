0d20b1df48385b218851082586b035b2df070369
Rick T. Zhang <rtzhang@ucsb.edu>
2023-03-17 04:51:14 +0000

    make main work

---
+++ src/main.h
@@ -0,0 +1,19 @@
+// main.h
+// @rt.z
+// @yarwinliu, in the end i admit, you had the better idea
+
+#ifndef MAIN
+#define MAIN
+
+#include <Arduino.h>
+
+enum States {ERROR, OFF, ON, ON_READY, DRIVE, CHARGE_PRECHARGE, CHARGE_CHARGING, CHARGE_FULL};
+
+//placeholder #includes for until the actual files come out
+/*
+#include "on_off.cpp"
+#include "drive.cpp"
+#include "charge.cpp"
+*/
+
+#endif
