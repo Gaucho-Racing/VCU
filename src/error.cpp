a7a6fdb52171cd35cf7e3255f312fb88f14ea74f
Th3will <wni1717@gmail.com>
2023-04-14 18:09:01 -0700

    Update error.cpp

--- src/error.cpp
+++ src/error.cpp
@@ -1,6 +1,7 @@
 #include "error.h"
 States error(I_no_can_speak_flex &car, States prevStates, volatile bool (*erCheck)(void)){
-    
+    car.DTI.setCurrent(0);
+    car.DTI.setDriveEnable(0);
     if(erCheck()){
         return ERROR;
     } else {
