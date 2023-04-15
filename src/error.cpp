09221b45e164a69f8171f77c79d99df461121136
Nikunj Parasar <nikunj.parasar@gmail.com>
2023-04-14 18:08:56 -0700

    error forgot

--- src/error.cpp
+++ src/error.cpp
@@ -1,5 +1,7 @@
 #include "error.h"
 States error(I_no_can_speak_flex &car, States prevStates, volatile bool (*erCheck)(void)){
+    car.DTI.setDriveEnable(0);
+    car.DTI.setCurrent(0);
     
     if(erCheck()){
         return ERROR;
