3d4b973ec2ff867149c95d510df7752c7b309ab2
Rick T. Zhang <rtzhang@ucsb.edu>
2023-04-20 16:40:46 +0000

    Integration to interrupts implementation: Moved loop control to outer loop Rewrote charge_precharge() Using CAN Charging struct data

--- src/charge.cpp
+++ src/charge.cpp
@@ -2,30 +2,28 @@
 // @rt.z
 
 #include "main.h"
+#include "constants.h"
 
 States charge_precharge(I_no_can_speak_flex &car) {
-   /* interrupts logic, @nikunjparasar please include */
-   delay(4000);
-   // loop get data/interrupt check 
-   return CHARGE_CHARGING;
+   if (car.charger.getVoltage() > CHARGING_VOLTAGE) return CHARGE_CHARGING;
+   if (car.charger.getCurrent() <= 0) return OFF;
+   return CHARGE_PRECHARGE;
 }
 
 States charge_charging(I_no_can_speak_flex &car) {
-   while (1) {
-      car.DTI.setRCurrent(0);
-      // get data
-      // push charge data to dash
-      if (car.BMS.getChargeState() > 99) return CHARGE_FULL;
-      if (car.BMS.getCurrent() < 0) return OFF;
-   }
+   car.DTI.setRCurrent(0);
+   // get data
+   // push charge data to dash
+   if (car.BMS.getChargeState() > 99) return CHARGE_FULL;
+   if (car.charger.getCurrent() < 0) return OFF;
+   return CHARGE_CHARGING;
 }
 
 States charge_full(I_no_can_speak_flex &car) {
-   while (1) {
-      // get data
-      car.DTI.setRCurrent(0);
-      // push charge data to dash
-      if (car.BMS.getCurrent() > 0 && car.BMS.getChargeState() <= 99) return CHARGE_CHARGING;
-      if (car.BMS.getCurrent() < 0) return OFF;
-   }
+   // get data
+   car.DTI.setRCurrent(0);
+   // push charge data to dash
+   if (car.charger.getCurrent() > 0 && car.BMS.getChargeState() <= 99) return CHARGE_CHARGING;
+   if (car.charger.getCurrent() < 0) return OFF;
+   return CHARGE_CHARGING;
 }
