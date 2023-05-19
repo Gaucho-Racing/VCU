2a07466bc4c7797848ba8b1bf23466d17fab4035
Rick T. Zhang <rtzhang@ucsb.edu>
2023-05-19 02:35:55 +0000

    off() rewrite version 10000.

--- src/off.cpp
+++ src/off.cpp
@@ -7,20 +7,15 @@
 
 // Starts at this state when the car is unlocked
 States off(I_no_can_speak_flex &car) {
-    bool rejectStartup = false;
-    if (isRejectingStartup(car)) rejectStartup = true;
-    // Start power to necessary things 
-    // Get whether on or off (some function) (see below if statement) 
-    if (onPressed(car) && !rejectStartup) {
+    car.DTI.setRCurrent(0);
+    if (onPressed(car)) {
         return ON;
     }
-    if (!onPressed(car) && !isRejectingStartup(car)) rejectStartup = false;
+    return OFF;
 
     /*
     if(car.BMS.getCurrent() > 0) {
         return CHARGE_PRECHARGE;
     } 
     */
-    
-    return OFF;
 }
